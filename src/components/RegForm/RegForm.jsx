a87575d8f36b61fd40b0cfb84a0d26220454bd5e
Igorsml <igorsmlff@gmail.com>
2023-11-11 14:08:19 +0300

    fix: form

--- src/components/RegForm/RegForm.jsx
+++ src/components/RegForm/RegForm.jsx
@@ -1,26 +1,43 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+
 import classes from "./RegForm.module.scss";
+
+const emailTestRegExp =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const emailValidator = [
+  (v) => !v.length && "поле email не может быть пустым",
+  (v) => !emailTestRegExp.test(v) && "некоректный email",
+];
+
+const passwordValidator = [
+  (v) => !v.length && "поле пароль не может быть пустым",
+  (v) => v.length < 3 && "пароль должен быть более 3 символов",
+  (v) => v.length >= 8 && "пароль должен быть менее 8 символов",
+];
+
+function testValue(value, tests) {
+  tests = [].concat(tests);
+
+  for (const test of tests) {
+    const result = test(value);
+
+    if (typeof result === "string") return result;
+  }
+
+  return null;
+}
 
 export const RegForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [emailDirty, setEmailDirty] = useState(false);
   const [passwordDirty, setPasswordDirty] = useState(false);
-  const [emailError, setEmailError] = useState(
-    "поле email не может быть пустым"
-  );
-  const [passwordError, setPasswordError] = useState(
-    "поле пароль не может быть пустым"
-  );
-  const [formValid, setFormValid] = useState(false);
 
-  useEffect(() => {
-    if (emailError || passwordError) {
-      setFormValid(false);
-    } else {
-      setFormValid(true);
-    }
-  }, [emailError, passwordError]);
+  const emailError = testValue(email, emailValidator);
+  const passwordError = testValue(password, passwordValidator);
+  const formValid =
+    emailDirty && passwordDirty && !emailError && !passwordError;
 
   const blurHandler = (event) => {
     switch (event.target.name) {
@@ -33,44 +50,13 @@ export const RegForm = () => {
     }
   };
 
-  const emailHandler = (event) => {
-    setEmail(event.target.value);
-
-    if (
-      !String(event.target.value)
-        .toLowerCase()
-        .match(
-          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        )
-    ) {
-      setEmailError("Некорректный email");
-    } else {
-      setEmailError("");
-    }
-  };
-
-  const passwordHandler = (event) => {
-    setPassword(event.target.value);
-
-    if (password.length < 3 || password.length >= 8) {
-      setPasswordError("Пароль должен быть длиннее 3 и меньше 8 символов");
-      if (!event.target.value) {
-        setPasswordError("поле пароль не может быть пустым");
-      }
-    } else {
-      setPasswordError("");
-    }
-  };
-
-  console.log(password.length);
-
   return (
     <form className={classes.formBody}>
       <h2>Регистрация</h2>
       <input
         className={classes.formInput}
         onBlur={blurHandler}
-        onChange={(e) => emailHandler(e)}
+        onChange={({ target }) => setEmail(target.value)}
         name="email"
         type="email"
         placeholder="Your email"
@@ -82,7 +68,7 @@ export const RegForm = () => {
       <input
         className={classes.formInput}
         onBlur={blurHandler}
-        onChange={(e) => passwordHandler(e)}
+        onChange={({ target }) => setPassword(target.value)}
         name="password"
         type="password"
         placeholder="Your password between 3 & 8 characters"
