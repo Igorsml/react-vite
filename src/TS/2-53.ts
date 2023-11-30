ee25042b924fb731a90cc8f721812e5675f702fc
Igorsml <igorsmlff@gmail.com>
2023-11-30 18:05:59 +0300

    feat: TS tasks

--- src/TS/2-53.ts
+++ src/TS/2-53.ts
@@ -508,11 +508,39 @@ const user: User = { login: 'login1' };
 const admin = addAdmin(user);
 console.log(admin); // { login: 'login1', permission: Permission.READ }
 
-// # 30
+
+// # 30 Реализуйте объект по описанному типу Form. Поле name.value должно проходить валидацию, а поле age — нет.
+console.clear()
+
+type Form = {
+  age: {
+    value: number,
+    validator: (val: number) => boolean
+  },
+  name: {
+    value: string,
+    validator: (val: string) => boolean
+  }
+};
+
+const form: Form = {
+  age: {
+    value: 23,
+    validator: (val: number) => val > 25,
+  },
+  name: {
+    value: "Igor",
+    validator: (val: string) => val.length >= 3,
+  }
+};
+
+
+console.log(form.name.validator(form.name.value)); // true
+console.log(form.age.validator(form.age.value)); // false
 
 
 // # 31
-
+  
 
 // # 32
 
